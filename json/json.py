a9f004e98d07d686dec0fa68ea06179dff51fb30
whlyc <33173982+whlyc@users.noreply.github.com>
2018-07-14 00:56:12 +0800

    Update json.py

--- json/json.py
+++ json/json.py
@@ -2,21 +2,22 @@ import os
 import json
 
 class Json(object):
-    def __init__(self):
+    def __init__(self,dir):
         self.dic = {}
-        if os.path.exists('test.json'):
-            with open('test.json','r') as load_f:
+        self.dir = dir
+        if os.path.exists(self.dir):
+            with open(self.dir,'r') as load_f:
                 self.dic = json.load(load_f) #dump
         else:
             print('no argments!')
             
     def update(self,scope,name,content):
         self.dic[scope][name] = content
-        with open('test.json','w') as f:
+        with open(self.dir,'w') as f:
             f.write(json.dumps(self.dic))
             
     def show(self):
         print(self.dic)
-    def set_dir(self,this):
+    
         
         
